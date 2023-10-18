618fb72d1d51aae9581ce8d32790c887576a7cf5
Habte Selassie <habtuchelsea1000@gmail.com>
2023-10-18 03:49:40 +0300

    update commit

--- index.js
+++ index.js
@@ -7,20 +7,21 @@ import Footer from "./Footer";
 import LotteryForm from "./LotteryForm";
 import LotteryFormSteps from "./LotteryFormSteps";
 import App from "./App";
+//import { Router } from "react-router-dom";
 import { Router } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 
 ReactDOM.createRoot(rootElement).render(
   <>
-  
-     <Navigation />    
+  <Router>
+      <Navigation />    
       <Body /> 
       <Testimonial />  
       <Footer />   
       <LotteryForm />   
       <LotteryFormSteps />
-       
+  </Router>  
   </>
 );
 
